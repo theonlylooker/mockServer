8ca54f3b851ddd962ccbd470ab9efe33b7322981
basurco eduardo <angelsoulsword@gmail.com>
2023-06-02 12:59:41 -0500

    feat: add initial routes

---
+++ index.js
@@ -0,0 +1,28 @@
+const jserver = require("json-server");
+const jauth = require("json-server-auth");
+const express = require("express");
+const places = require("./places.json");
+const users = require("./users.json");
+const wishlist = require("./wishlist.json");
+
+const app = jserver.create();
+const ownRouter = jserver.router({
+  places,
+  users,
+  wishlist,
+});
+
+const middlewares = jserver.defaults();
+
+const port = process.env.PORT || 3000;
+
+// /!\ Bind the router db to the app
+app.db = ownRouter.db;
+
+// You must apply the auth middleware before the router
+app.use(middlewares);
+app.use(jauth);
+app.use(ownRouter);
+app.listen(port, () => {
+  console.log("App started on", port);
+});
